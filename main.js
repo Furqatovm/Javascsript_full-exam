bb1944a533b50cafc25dda75f2386b064107f969
Furqatovm <muhammadyusuffurqatov91@gmail.com>
2025-08-03 19:48:05 +0500

    talet is ready

--- main.js
+++ main.js
@@ -2,16 +2,19 @@ let header =document.getElementById("header");
 let menu =document.getElementById("menu");
 
 let closee =document.getElementById("close");
-closee.style.cursor ="pointer"
+closee.style.cursor ="pointer";
+let qora =document.getElementById("qora");
 
 let nav =document.getElementById("nav");
 
 menu.addEventListener("click", ()=> {
     header.style.display ="none";
     nav.style.display ="flex";
+    qora.style.display ="block"
 });
 
 closee.addEventListener("click", ()=> {
     header.style.display ="flex";
     nav.style.display ="none";
+    qora.style.display ="none"
 })
